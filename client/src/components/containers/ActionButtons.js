45eeb7426d56b279ec2ceab53b035a7e0bb14bbe
mohsenoffice <mohsen.office@gmail.com>
2020-02-11 04:19:53 +0200

    add the functionality to create a random users

--- client/src/components/containers/ActionButtons.js
+++ client/src/components/containers/ActionButtons.js
@@ -3,6 +3,8 @@ import {Redirect} from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import { submitUser } from '../../actions/actions';
+import { connect } from 'react-redux';
+import { withRouter } from "react-router-dom";
 
 class ActionButtons extends Component {
 
@@ -15,38 +17,35 @@ class ActionButtons extends Component {
     };
   }
 
-
-
-  generateRandom(){
-    let randomSubmission = Object.assign({}, this.state.random);
-    //alert(this.props.submission.name);
-
-    randomSubmission["name"] = "mohsen";
-    randomSubmission["mail"] = "mohsen";
-    randomSubmission["address"] = "mohsen";
-    //randomSubmission["birthday"] = randomName();
-   // alert(updatedSubmission[event.target.id]);
-    this.setState({
-        submission: randomSubmission   
-    });
-  }
+ 
 
 
 
   submitSubmission(){
-    this.generateRandom();
-    //this.props.dispatch(submitUser(this.state.random));    
-    //this.props.history.push("/");
+    let randomSubmission = Object.assign({}, this.state.random);
+    const rand = Math.floor(Math.random() * 100000);
+    randomSubmission["name"] = "Mohsen " + rand;
+    randomSubmission["mail"] = "mohsen"+ rand +"@gmail.com";
+    randomSubmission["address"] = "Massada "+rand;
+    randomSubmission["birthday"] = new Date(+(new Date()) - Math.floor(Math.random()*100000000000));
+    this.props.dispatch(submitUser(randomSubmission));    
+    window.location.reload();
   }
 
   render(){
     return (
-      <div class="text-center">
+      <div class="text-center m-3">
         <Link to="/add" className="btn btn-success">New</Link>
-        <Button variant="warning mx-1">Random</Button>
+        <Button variant="warning mx-1" onClick={this.submitSubmission.bind(this)} >Random</Button>
         <Button variant="primary mx-1">Compare</Button>
+    
       </div>
   )}
 }
 
-export default ActionButtons;
+const mapStateToProps = state => {
+  return {
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(ActionButtons));
--- client/src/components/containers/Users.js
+++ client/src/components/containers/Users.js
@@ -2,6 +2,7 @@ import React, { Component} from 'react';
 import UsersItemListing from '../presentation/UsersItemListing';
 import { connect } from 'react-redux'
 import { fetchUsers } from '../../actions/actions'
+import ActionButtons from './ActionButtons';
 
 class Users extends Component {
     componentDidMount(){
@@ -18,6 +19,7 @@ class Users extends Component {
 
         return (
             <div>
+                <ActionButtons  />
                 <ul>
                 {(this.props.users.length > 0) ? <ul>{usersItems}</ul> : <div>Sorry we have no users</div>}
                 </ul>
